a28c0c0b658992618552943455e26650bb46dbca
Babatunde Adeyemi <barbietunnie@gmail.com>
2017-01-27 10:24:36 +0100

    Add Book model

--- commands.sh
+++ commands.sh
@@ -1,1 +1,2 @@
 sequelize model:create --name=Author --attributes name:string,bio:text --underscored
+sequelize model:create --name=Book --attributes name:string,isbn:integer,publication_date:date,description:text,author_id:integer --underscore
---
+++ server/migrations/20170127092324-create-book.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      isbn: {
+        type: Sequelize.INTEGER
+      },
+      publication_date: {
+        type: Sequelize.DATE
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      author_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Books');
+  }
+};
